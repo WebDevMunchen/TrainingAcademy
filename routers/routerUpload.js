9e93a166d94abadb20d4569979729cbd4a1631f6
unknown <webdevmunchen@gmail.com>
2024-08-07 07:41:22 +0200

    cleaned up console logs and comments

--- routers/routerUpload.js
+++ routers/routerUpload.js
@@ -13,7 +13,6 @@ router.post("/upload", upload.single("image"), function (req, res) {
 
   cloudinary.uploader.upload(req.file.path, function (err, result) {
     if (err) {
-      console.log(err);
       return res.status(500).json({
         success: false,
         message: "Error",
