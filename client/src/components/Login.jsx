d850543bd70ec350e360f1dabae6f4a7c17b62c3
unknown <denis991280@gmail.com>
2024-11-29 14:18:23 +0100

    minor changes to the ui

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -29,9 +29,9 @@ export default function Login() {
       </div>
 
       <section>
-        <div className="flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="flex flex-col items-center px-3 py-8 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <div className="p-4 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="font-poppins text-md font-bold leading-tight tracking-tight text-gray-900 md:text-xl dark:text-white">
                 Melde dich hier mit deinem Konto an:
               </h1>
@@ -130,7 +130,7 @@ export default function Login() {
                         d="M11.983,0a12.206,12.206,0,0,0-8.51,3.653A11.8,11.8,0,0,0,0,12.207,11.779,11.779,0,0,0,11.8,24h.214A12.111,12.111,0,0,0,24,11.791h0A11.766,11.766,0,0,0,11.983,0ZM10.5,16.542a1.476,1.476,0,0,1,1.449-1.53h.027a1.527,1.527,0,0,1,1.523,1.47,1.475,1.475,0,0,1-1.449,1.53h-.027A1.529,1.529,0,0,1,10.5,16.542ZM11,12.5v-6a1,1,0,0,1,2,0v6a1,1,0,1,1-2,0Z"
                       ></path>
                     </svg>
-                    <span className="flex justify-center w-full text-red-800 font-semibold">
+                    <span className="flex font-[15px] justify-center w-full text-red-800 font-semibold">
                       {" "}
                       Anmeldung fehlgeschlagen{" "}
                     </span>
